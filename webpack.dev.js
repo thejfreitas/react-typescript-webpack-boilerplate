35fcc311abd63f1dcba31b6782e9a2ecbc0125e7
J. de Freitas <juniormfreitas@gmail.com>
2021-11-06 16:00:41 -0600

    Set basic configuration on Webpack to development and production

---
+++ webpack.dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  devServer: {
+    hot: true,
+    open: true,
+  },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,7 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  devtool: false,
+};
